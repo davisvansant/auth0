9c89aeffde35b90bdef64fa9ad0204c17fb3368c
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-08-29 16:02:19 -0700

    initial passwordless api endpoints

--- authentication/src/lib.rs
+++ authentication/src/lib.rs
@@ -31,6 +31,7 @@ mod tests {
     use crate::login::Social;
     use crate::logout::Logout;
     use crate::logout::RequestParameters;
+    use crate::passwordless::*;
     #[test]
     fn it_works() {
         let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
@@ -50,9 +51,36 @@ mod tests {
             client_id: String::from("some_awesome_client_id"),
             federated: String::from("some_awesome_federated"),
         };
+        let passwordless_code = passwordless::CodeOrLink {
+            client_id: String::from("some_awesome_client_id"),
+            client_secret: String::from("some_awesome_client_secret"),
+            connection: String::from("some_awesome_connection"),
+            email: String::from("tester@awesome.com"),
+            phone_number: String::from("000000000"),
+            send: String::from("some_awesome_link_to_send"),
+            auth_params: String::from("some_awesome_auth_params"),
+        };
+        let passwordless_login = passwordless::AuthenticateUser {
+            grant_type: String::from("some_awesome_grant_type"),
+            client_id: String::from("some_awesome_client_id"),
+            client_secret: String::from("some_awesome_client_secret"),
+            username: String::from("some_awesome_username"),
+            realm: String::from("some_awesome_realm"),
+            otp: String::from("some_awesome_otp"),
+            audience: String::from("some_awesome_audience"),
+            scope: String::from("some_awesome_scope"),
+        };
         let parameters = login::LoginRequest::collect(login::AuthenicationType::Social(social));
         let logout_parameters = logout::LogoutRequest::collect(logout);
+        let passwordless_code_parameters = passwordless::PasswordlessRequest::collect(
+            passwordless::RequestType::CodeOrLink(passwordless_code),
+        );
+        let passwordless_login_parameters = passwordless::PasswordlessRequest::collect(
+            passwordless::RequestType::AuthenticateUser(passwordless_login),
+        );
         management.authorize(parameters);
         management.logout(logout_parameters);
+        management.passwordless_start(passwordless_code_parameters);
+        management.passwordless_login(passwordless_login_parameters);
     }
 }
--- authentication/src/passwordless.rs
+++ authentication/src/passwordless.rs
@@ -1,1 +1,66 @@
-//
+use crate::Api;
+use reqwest::RequestBuilder;
+use serde::{Deserialize, Serialize};
+use std::collections::BTreeMap;
+
+#[derive(Serialize, Deserialize)]
+pub enum RequestType {
+    CodeOrLink(CodeOrLink),
+    AuthenticateUser(AuthenticateUser),
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CodeOrLink {
+    pub client_id: String,
+    pub client_secret: String,
+    pub connection: String,
+    pub email: String,
+    pub phone_number: String,
+    pub send: String,
+    #[serde(rename(serialize = "authParams"))]
+    pub auth_params: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct AuthenticateUser {
+    pub grant_type: String,
+    pub client_id: String,
+    pub client_secret: String,
+    pub username: String,
+    pub realm: String,
+    pub otp: String,
+    pub audience: String,
+    pub scope: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct PasswordlessRequest {
+    parameters: RequestType,
+}
+
+impl PasswordlessRequest {
+    pub fn collect(parameters: RequestType) -> PasswordlessRequest {
+        PasswordlessRequest { parameters }
+    }
+}
+
+pub trait Passwordless {
+    fn passwordless_start(&self, parameters: PasswordlessRequest) -> RequestBuilder;
+    fn passwordless_login(&self, parameters: PasswordlessRequest) -> RequestBuilder;
+}
+
+impl Passwordless for Api {
+    fn passwordless_start(&self, passwordless_request: PasswordlessRequest) -> RequestBuilder {
+        let client = reqwest::Client::new();
+        let endpoint = String::from("/passwordless/start");
+        let url = self.base_url.join(&endpoint).unwrap();
+        client.post(url).json(&passwordless_request.parameters)
+    }
+
+    fn passwordless_login(&self, passwordless_request: PasswordlessRequest) -> RequestBuilder {
+        let client = reqwest::Client::new();
+        let endpoint = String::from("/oath/token");
+        let url = self.base_url.join(&endpoint).unwrap();
+        client.post(url).json(&passwordless_request.parameters)
+    }
+}
