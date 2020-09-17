10be81449d71df110c3a5094728785c10c0cc2fa
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-17 16:44:07 -0700

    initial tests for passwordless, updating request url

--- authentication/src/passwordless.rs
+++ authentication/src/passwordless.rs
@@ -20,8 +20,81 @@ impl Passwordless for Api {
 
     fn passwordless_login(&self, request: authenticate_user::RequestParameters) -> RequestBuilder {
         let client = reqwest::Client::new();
-        let endpoint = String::from("/oath/token");
+        let endpoint = String::from("/oauth/token");
         let url = self.base_url.join(&endpoint).unwrap();
         client.post(url).json(&request)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn passwordless_start_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let passwordless = Api::init(base_url, authentication);
+        let parameters = passwordless::get_code_or_link::RequestParameters {
+            client_id: String::from("some_awesome_client_id"),
+            client_secret: String::from("some_awesome_client_secret"),
+            connection: String::from("some_awesome_connection"),
+            email: Some(String::from("tester@awesome.com")),
+            phone_number: None,
+            send: None,
+            auth_params: None,
+        };
+        let request = passwordless.passwordless_start(parameters).build().unwrap();
+        let test_url = String::from("https://your_domain/passwordless/start");
+        let test_body = String::from(
+            "{\"client_id\":\"some_awesome_client_id\",\
+            \"client_secret\":\"some_awesome_client_secret\",\
+            \"connection\":\"some_awesome_connection\",\
+            \"email\":\"tester@awesome.com\"}",
+        );
+
+        assert_eq!(request.method().as_str(), reqwest::Method::POST);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().len(), 1);
+        assert_eq!(
+            request.body().unwrap().as_bytes().unwrap(),
+            test_body.as_bytes(),
+        );
+    }
+
+    #[test]
+    fn passwordless_login_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let passwordless = Api::init(base_url, authentication);
+        let parameters = passwordless::authenticate_user::RequestParameters {
+            grant_type: String::from("some_awesome_grant_type"),
+            client_id: String::from("some_awesome_client_id"),
+            client_secret: String::from("some_awesome_client_secret"),
+            username: String::from("some_awesome_username"),
+            realm: String::from("some_awesome_realm"),
+            otp: String::from("some_awesome_otp"),
+            audience: None,
+            scope: None,
+        };
+        let request = passwordless.passwordless_login(parameters).build().unwrap();
+        let test_url = String::from("https://your_domain/oauth/token");
+        let test_body = String::from(
+            "{\"grant_type\":\"some_awesome_grant_type\",\
+            \"client_id\":\"some_awesome_client_id\",\
+            \"client_secret\":\"some_awesome_client_secret\",\
+            \"username\":\"some_awesome_username\",\
+            \"realm\":\"some_awesome_realm\",\
+            \"otp\":\"some_awesome_otp\"}",
+        );
+
+        assert_eq!(request.method().as_str(), reqwest::Method::POST);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().len(), 1);
+        assert_eq!(
+            request.body().unwrap().as_bytes().unwrap(),
+            test_body.as_bytes(),
+        );
     }
 }
