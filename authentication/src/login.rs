b69caf47425baca7b879cdcab73bbe0705ffa041
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-17 15:26:19 -0700

    initial tests for login

--- authentication/src/login.rs
+++ authentication/src/login.rs
@@ -18,3 +18,74 @@ impl Login for Api {
         client.get(url).query(&request)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn enterprise_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let login = Api::init(base_url, authentication);
+        let parameters = login::enterprise::RequestParamaters {
+            response_type: String::from("some_awesome_response_type"),
+            client_id: String::from("some_awesome_client_id"),
+            connection: None,
+            redirect_uri: String::from("some_awesome_redirect_uri"),
+            state: None,
+        };
+        let request = login.authorize(parameters).build().unwrap();
+        let test_url =
+            String::from("https://your_domain/authorize?response_type=some_awesome_response_type&client_id=some_awesome_client_id&redirect_uri=some_awesome_redirect_uri");
+        assert_eq!(request.method().as_str(), reqwest::Method::GET);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().is_empty(), true);
+        assert_eq!(request.body().is_none(), true);
+    }
+
+    #[test]
+    fn passive_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let login = Api::init(base_url, authentication);
+        let parameters = login::passive::RequestParamaters {
+            response_type: String::from("some_awesome_response_type"),
+            client_id: String::from("some_awesome_client_id"),
+            connection: None,
+            redirect_uri: String::from("some_awesome_redirect_uri"),
+            scope: None,
+            state: Some(String::from("some_awesome_state")),
+        };
+        let request = login.authorize(parameters).build().unwrap();
+        let test_url =
+            String::from("https://your_domain/authorize?response_type=some_awesome_response_type&client_id=some_awesome_client_id&redirect_uri=some_awesome_redirect_uri&state=some_awesome_state");
+        assert_eq!(request.method().as_str(), reqwest::Method::GET);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().is_empty(), true);
+        assert_eq!(request.body().is_none(), true);
+    }
+
+    #[test]
+    fn social_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let login = Api::init(base_url, authentication);
+        let parameters = login::social::RequestParamaters {
+            response_type: String::from("some_awesome_response_type"),
+            client_id: String::from("some_awesome_client_id"),
+            connection: None,
+            redirect_uri: String::from("some_awesome_redirect_uri"),
+            state: None,
+            additional_parameters: None,
+        };
+        let request = login.authorize(parameters).build().unwrap();
+        let test_url =
+            String::from("https://your_domain/authorize?response_type=some_awesome_response_type&client_id=some_awesome_client_id&redirect_uri=some_awesome_redirect_uri");
+        assert_eq!(request.method().as_str(), reqwest::Method::GET);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().is_empty(), true);
+        assert_eq!(request.body().is_none(), true);
+    }
+}
