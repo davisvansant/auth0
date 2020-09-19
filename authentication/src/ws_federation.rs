1b68f9f5f8171fce4e9e6fe8b8cdf3c1226b8bd7
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-19 15:33:40 -0700

    initial tests for ws_federation

--- authentication/src/ws_federation.rs
+++ authentication/src/ws_federation.rs
@@ -26,3 +26,44 @@ impl WSFederation for Api {
         client.get(url)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::*;
+
+    #[test]
+    fn accept_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let ws_federation = Api::init(base_url, authentication);
+        let parameters = ws_federation::accept_request::RequestParameters {
+            client_id: String::from("some_awesome_client_id"),
+            wtrealm: None,
+            whr: None,
+            wctx: None,
+            wreply: None,
+        };
+        let request = ws_federation.accept_request(parameters).build().unwrap();
+        let test_url = String::from("https://your_domain/wsfed/some_awesome_client_id");
+        assert_eq!(request.method().as_str(), reqwest::Method::GET);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().len(), 0);
+        assert_eq!(request.body().is_none(), true);
+    }
+
+    #[test]
+    fn get_metadata_request() {
+        let base_url = Url::parse("https://YOUR_DOMAIN").unwrap();
+        let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
+        let ws_federation = Api::init(base_url, authentication);
+        let request = ws_federation.get_metadata().build().unwrap();
+        let test_url = String::from(
+            "https://your_domain/wsfed/FederationMetadata/2007-06/FederationMetadata.xml",
+        );
+        assert_eq!(request.method().as_str(), reqwest::Method::GET);
+        assert_eq!(request.url().as_str(), test_url);
+        assert_eq!(request.headers().len(), 0);
+        assert_eq!(request.body().is_none(), true);
+    }
+}
