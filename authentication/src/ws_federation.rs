d46e8bab37766959902a574075dd06bd2846561f
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-24 11:47:09 -0700

    adjust ws federation to use self.client

--- authentication/src/ws_federation.rs
+++ authentication/src/ws_federation.rs
@@ -11,19 +11,17 @@ pub trait WSFederation {
 
 impl WSFederation for Api {
     fn accept_request(&self, request: accept_request::RequestParameters) -> RequestBuilder {
-        let client = reqwest::Client::new();
         let endpoint = String::from("/wsfed/");
         let base_url = self.base_url.join(&endpoint).unwrap();
         let url = base_url.join(&request.client_id).unwrap();
 
-        client.get(url).query(&request)
+        self.client.get(url).query(&request)
     }
 
     fn get_metadata(&self) -> RequestBuilder {
-        let client = reqwest::Client::new();
         let endpoint = String::from("/wsfed/FederationMetadata/2007-06/FederationMetadata.xml");
         let url = self.base_url.join(&endpoint).unwrap();
-        client.get(url)
+        self.client.get(url)
     }
 }
 
