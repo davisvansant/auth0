35ff9a65d8acefa8bd0d670920e9a210494baedf
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-27 21:11:01 -0700

    initial mocks for logout tests, with minor cleanup

--- authentication/tests/logout_test.rs
+++ authentication/tests/logout_test.rs
@@ -3,7 +3,14 @@ use authentication::*;
 
 #[tokio::test]
 async fn logout_send_request() {
-    let base_url = reqwest::Url::parse("https://YOUR_DOMAIN").unwrap();
+    let mock = mockito::mock("GET", "/v2/logout")
+        .match_query(mockito::Matcher::AllOf(vec![
+            mockito::Matcher::UrlEncoded("returnTo".into(), "some_awesome_return".into()),
+            mockito::Matcher::UrlEncoded("client_id".into(), "some_awesome_client_id".into()),
+            mockito::Matcher::UrlEncoded("federated".into(), "some_awesome_federated".into()),
+        ]))
+        .create();
+    let base_url = reqwest::Url::parse(&mockito::server_url()).unwrap();
     let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
     let logout = Api::init(base_url, authentication);
     let test_parameters = logout::RequestParameters {
@@ -11,7 +18,8 @@ async fn logout_send_request() {
         client_id: Some(String::from("some_awesome_client_id")),
         federated: Some(String::from("some_awesome_federated")),
     };
-    let test_response = send_request(logout.logout(test_parameters)).await;
-    assert!(test_response.is_err());
-    assert!(test_response.unwrap_err().is_request());
+    let test_response = logout.logout(test_parameters).send().await;
+    mock.assert();
+    assert!(mock.matched());
+    assert_eq!(test_response.unwrap().status(), reqwest::StatusCode::OK);
 }
