3e2e8c0292bd2c16c5ac964d17923ec7225d69a3
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-29 12:53:43 -0700

    initial mocks for ws federation

--- authentication/tests/authentication_ws_federation_test.rs
+++ authentication/tests/authentication_ws_federation_test.rs
@@ -1,9 +1,11 @@
 use authentication::ws_federation::*;
 use authentication::*;
+use mockito::mock;
 
 #[tokio::test]
 async fn accept_request_send_request() {
-    let base_url = reqwest::Url::parse("https://YOUR_DOMAIN").unwrap();
+    let mock = mock("GET", "/wsfed/some_awesome_client_id").create();
+    let base_url = reqwest::Url::parse(&mockito::server_url()).unwrap();
     let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
     let ws_federation = Api::init(base_url, authentication);
     let test_parameters = ws_federation::accept_request::RequestParameters {
@@ -14,16 +16,23 @@ async fn accept_request_send_request() {
         wreply: None,
     };
     let test_response = ws_federation.accept_request(test_parameters).send().await;
-    assert!(test_response.is_err());
-    assert!(test_response.unwrap_err().is_request());
+    mock.assert();
+    assert!(mock.matched());
+    assert_eq!(test_response.unwrap().status(), reqwest::StatusCode::OK);
 }
 
 #[tokio::test]
 async fn get_metadata_send_request() {
-    let base_url = reqwest::Url::parse("https://YOUR_DOMAIN").unwrap();
+    let mock = mock(
+        "GET",
+        "/wsfed/FederationMetadata/2007-06/FederationMetadata.xml",
+    )
+    .create();
+    let base_url = reqwest::Url::parse(&mockito::server_url()).unwrap();
     let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
     let ws_federation = Api::init(base_url, authentication);
     let test_response = ws_federation.get_metadata().send().await;
-    assert!(test_response.is_err());
-    assert!(test_response.unwrap_err().is_request());
+    mock.assert();
+    assert!(mock.matched());
+    assert_eq!(test_response.unwrap().status(), reqwest::StatusCode::OK);
 }
