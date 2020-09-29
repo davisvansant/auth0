0baa372a4fe0c84c46778396da3a646439b8105f
Davis Van Sant <davisvansant@users.noreply.github.com>
2020-09-29 12:15:01 -0700

    minor user profile test cleanup, including initial mocks

--- authentication/tests/authentication_user_profile_test.rs
+++ authentication/tests/authentication_user_profile_test.rs
@@ -1,15 +1,20 @@
 use authentication::user_profile::*;
 use authentication::*;
+use mockito::mock;
 
 #[tokio::test]
 async fn user_profile_send_request() {
-    let base_url = reqwest::Url::parse("https://YOUR_DOMAIN").unwrap();
+    let mock = mock("GET", "/userinfo")
+        .match_header("authorization", "Bearer some_awesome_access_token")
+        .create();
+    let base_url = reqwest::Url::parse(&mockito::server_url()).unwrap();
     let authentication = AuthenicationMethod::OAuth2Token(String::from("some_awesome_token"));
     let user_profile = Api::init(base_url, authentication);
     let test_parameters = user_profile::RequestParameters {
         access_token: String::from("some_awesome_access_token"),
     };
-    let test_response = send_request(user_profile.user_info(test_parameters)).await;
-    assert!(test_response.is_err());
-    assert!(test_response.unwrap_err().is_request());
+    let test_response = user_profile.user_info(test_parameters).send().await;
+    mock.assert();
+    assert!(mock.matched());
+    assert_eq!(test_response.unwrap().status(), reqwest::StatusCode::OK);
 }
